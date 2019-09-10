c543a759c04d3ccc42b701f2d398d64532547718
WhiplashGam <48991638+WhiplashGam@users.noreply.github.com>
2019-09-10 14:24:21 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const fs = require('fs');
+
+client.login(process.env.TOKEN);
+
+client.commands = new Discord.Collection();
+
+fs.readdir("./Commandes/",(error, f) => {
+    if(error) console.log(error);
+
+    let Commandes = f.filter(f => f.split(".").pop() === "js");
+    if(Commandes.length <= 0) return console.log("Aucune commandes trouvée !");
+
+    Commandes.forEach((f) => {
+
+        let commande = require(`./Commandes/${f}`);
+        console.log(`${f} commande chargée !`);
+
+    client.commands.set(commande.help.name, commande);    
+    });
+
+});
+
+fs.readdir("./Events/", (error, f) => {
+    if(error) console.log(error);
+    console.log(`${f.length} events en chargement`);
+
+    f.forEach((f) => {
+        const events = require(`./Events/${f}`);
+        const event = f.split(".")[0];
+    
+    client.on(event, events.bind(null, client));
+    });
+});
+
+/*Partie Message Joint et Role Des Nouveaux*/
+
+client.on('guildMemberAdd', member =>{
+    let embed = new Discord.RichEmbed()
+        .setDescription('**Bienvenue à toi mon petit **' + member.user.username + '**Sur le discord de la WhiplashFamilly**')
+    member.guild.channels.get('517613093719900162').send(embed)
+    member.addRole('517613763420094465')
+});
+
